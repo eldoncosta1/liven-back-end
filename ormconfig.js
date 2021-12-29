5fcebe073b864bc51da0df0d62b8c37267d654f9
Eldon de Aquino Costa <eldon.costa@compasso.com.br>
2021-12-29 10:59:07 -0300

    bugfix: correção arquivo conexão postgres

--- ormconfig.js
+++ ormconfig.js
@@ -24,11 +24,7 @@ if (process.env.environment === 'local') {
     {
       "name": "default",
       "type": "postgres",
-      "host": process.env.POSTGRES_HOST,
-      "port": process.env.POSTGRES_PORT,
-      "username": process.env.POSTGRES_USERNAME,
-      "password": process.env.POSTGRES_PASSWORD,
-      "database": process.env.POSTGRES_DB,
+      "url": process.env.DATABASE_URL,
       "autoLoadEntities": true,
       "ssl": {
         "rejectUnauthorized": false
