2dc469509d4a9b9ec7000019c610a1ab0c53cd34
Eldon de Aquino Costa <eldon.costa@compasso.com.br>
2021-12-29 10:35:47 -0300

    bugfix: correção da aplicação para deploy no heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,20 +1,43 @@
-module.exports = [
-  {
-    "name": "default",
-    "type": "postgres",
-    "host": process.env.POSTGRES_HOST,
-    "port": process.env.POSTGRES_PORT,
-    "username": process.env.POSTGRES_USERNAME,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DB,
-    "entities": [
-      "./src/modules/**/infra/typeorm/entities/*.ts"
-    ],
-    "migrations": [
-      "./src/shared/infra/typeorm/migrations/*.ts"
-    ],
-    "cli": {
-      "migrationsDir": "./src/shared/infra/typeorm/migrations"
+if (process.env.environment === 'local') {
+  module.exports = [
+    {
+      "name": "default",
+      "type": "postgres",
+      "host": process.env.POSTGRES_HOST,
+      "port": process.env.POSTGRES_PORT,
+      "username": process.env.POSTGRES_USERNAME,
+      "password": process.env.POSTGRES_PASSWORD,
+      "database": process.env.POSTGRES_DB,
+      "entities": [
+        "./src/modules/**/infra/typeorm/entities/*.ts"
+      ],
+      "migrations": [
+        "./src/shared/infra/typeorm/migrations/*.ts"
+      ],
+      "cli": {
+        "migrationsDir": "./src/shared/infra/typeorm/migrations"
+      }
     }
-  }
-]
+  ]
+} else {
+  module.exports = [
+    {
+      "name": "default",
+      "type": "postgres",
+      "host": process.env.POSTGRES_HOST,
+      "port": process.env.POSTGRES_PORT,
+      "username": process.env.POSTGRES_USERNAME,
+      "password": process.env.POSTGRES_PASSWORD,
+      "database": process.env.POSTGRES_DB,
+      "entities": [
+        "./dist/modules/**/infra/typeorm/entities/*.js"
+      ],
+      "migrations": [
+        "./dist/shared/**/infra/typeorm/migrations/*.js"
+      ],
+      "cli": {
+        "migrationsDir": "./src/shared/infra/typeorm/migrations"
+      }
+    }
+  ]
+}
